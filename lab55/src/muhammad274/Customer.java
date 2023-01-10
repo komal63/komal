d94fb10a5efcfd0b0cce0728add388737dbd3dbb
komal63 <76747241+komal63@users.noreply.github.com>
2023-01-10 03:06:57 -0700

    Add files via upload

---
+++ lab55/src/muhammad274/Customer.java
@@ -0,0 +1,15 @@
+package muhammad274;
+
+public class Customer {
+
+
+    public static void main(String[] args)
+    {
+
+        main obj = new main();
+ 
+        obj.withdrawn("Samra Faisal:", 45000);
+        obj.withdrawn("Tabeena:", 20000);
+    }
+
+}
---
+++ lab55/src/muhammad274/main.java
@@ -0,0 +1,51 @@
+package muhammad274;
+public class main {
+	int total = 50000;
+ 
+	synchronized void withdrawn(String name, int withdrawal) {
+		if (total >= withdrawal) {
+			System.out.println(name + " withdrawn " + withdrawal);
+
+			total = total - withdrawal;
+			System.out.println("Balance after withdrawal: " + total);
+
+			try {
+
+				Thread.sleep(1000);
+			}
+
+			catch (InterruptedException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+		else {
+
+			System.out.println(name + " you can not withdraw " + withdrawal);
+
+			System.out.println("your balance is: " + total);
+
+			try {
+				Thread.sleep(1000);
+			}
+
+			catch (InterruptedException e) {
+
+				e.printStackTrace();
+			}
+		}
+	}
+
+	synchronized void deposit(String name, int deposit) {
+		System.out.println(name + " deposited " + deposit);
+		total = total + deposit;
+		System.out.println("Balance after deposit: " + total);
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
